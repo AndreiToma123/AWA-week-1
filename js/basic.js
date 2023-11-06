e11a50a67e101722b25aa36df6ab74f6caf06a6f
AndreiToma123 <118735556+AndreiToma123@users.noreply.github.com>
2023-11-06 20:08:39 +0200

    feature 2

--- js/basic.js
+++ js/basic.js
@@ -8,7 +8,9 @@ if(document.readyState !== "loading"){
     })
 }
 
-function initializeCode(){
+async function initializeCode(){
+const response1 = await fetch('https://dog.ceo/api/breed/akita/images/random');
+const data1 = await response1.json();
 const wikiContainer1 = document.getElementById("item1");
 const wikiContainer2 = document.getElementById("item2");
 const wikiContainer3 = document.getElementById("item3");
@@ -24,13 +26,13 @@ let image = document.createElement("img");
 
 wikiItem.className = "wiki-item";
 h.className = "wiki-header";
-h.textContent = "Breed 1";
+h.textContent = "Akita";
 divContent.className = "wiki-content";
 paragraph.className = "wiki-text";
 paragraph.textContent = "Some text about this breed.";
 divImage.className = "img-container";
 image.className = "wiki-img";
-image.src = "";
+image.src = data1.message;
 
 divImage.appendChild(image);
 divContent.appendChild(paragraph);
@@ -40,6 +42,8 @@ wikiItem.appendChild(divContent);
 
 wikiContainer1.appendChild(wikiItem);
 // ----------------------------------------------
+const response2 = await fetch('https://dog.ceo/api/breed/beagle/images/random');
+const data2 = await response2.json();
 let wikiItem2 = document.createElement("div");
 let divContent2 = document.createElement("div");
 let divImage2 = document.createElement("div");
@@ -49,13 +53,13 @@ let image2 = document.createElement("img");
 
 wikiItem2.className = "wiki-item";
 h2.className = "wiki-header";
-h2.textContent = "Breed 1";
+h2.textContent = "Beagle";
 divContent2.className = "wiki-content";
 paragraph2.className = "wiki-text";
 paragraph2.textContent = "Some text about this breed.";
 divImage2.className = "img-container";
 image2.className = "wiki-img";
-image2.src = "";
+image2.src = data2.message;
 
 divImage2.appendChild(image2);
 divContent2.appendChild(paragraph2);
@@ -65,7 +69,8 @@ wikiItem2.appendChild(divContent2);
 
 wikiContainer2.appendChild(wikiItem2);
 // -------------------------------------------------------
-
+const response3 = await fetch('https://dog.ceo/api/breed/boxer/images/random');
+const data3 = await response3.json();
 let wikiItem3 = document.createElement("div");
 let divContent3 = document.createElement("div");
 let divImage3 = document.createElement("div");
@@ -75,13 +80,13 @@ let image3 = document.createElement("img");
 
 wikiItem3.className = "wiki-item";
 h3.className = "wiki-header";
-h3.textContent = "Breed 1";
+h3.textContent = "Boxer";
 divContent3.className = "wiki-content";
 paragraph3.className = "wiki-text";
 paragraph3.textContent = "Some text about this breed.";
 divImage3.className = "img-container";
 image3.className = "wiki-img";
-image3.src = "";
+image3.src = data3.message;
 
 divImage3.appendChild(image3);
 divContent3.appendChild(paragraph3);
@@ -91,7 +96,8 @@ wikiItem3.appendChild(divContent3);
 
 wikiContainer2.appendChild(wikiItem3);
 // ------------------------------------------
-
+const response4 = await fetch('https://dog.ceo/api/breed/chihuahua/images/random');
+const data4 = await response4.json();
 let wikiItem4 = document.createElement("div");
 let divContent4 = document.createElement("div");
 let divImage4 = document.createElement("div");
@@ -101,13 +107,13 @@ let image4 = document.createElement("img");
 
 wikiItem4.className = "wiki-item";
 h4.className = "wiki-header";
-h4.textContent = "Breed 1";
+h4.textContent = "Chihuahua";
 divContent4.className = "wiki-content";
 paragraph4.className = "wiki-text";
 paragraph4.textContent = "Some text about this breed.";
 divImage4.className = "img-container";
 image4.className = "wiki-img";
-image4.src = "";
+image4.src = data4.message;
 
 divImage4.appendChild(image4);
 divContent4.appendChild(paragraph4);
@@ -117,6 +123,8 @@ wikiItem4.appendChild(divContent4);
 
 wikiContainer4.appendChild(wikiItem4);
 //----------------------------------------------
+const response5 = await fetch('https://dog.ceo/api/breed/chow/images/random');
+const data5 = await response5.json();
 let wikiItem5 = document.createElement("div");
 let divContent5 = document.createElement("div");
 let divImage5 = document.createElement("div");
@@ -126,13 +134,13 @@ let image5 = document.createElement("img");
 
 wikiItem5.className = "wiki-item";
 h5.className = "wiki-header";
-h5.textContent = "Breed 1";
+h5.textContent = "Chow";
 divContent5.className = "wiki-content";
 paragraph5.className = "wiki-text";
 paragraph5.textContent = "Some text about this breed.";
 divImage5.className = "img-container";
 image5.className = "wiki-img";
-image5.src = "";
+image5.src = data5.message;
 
 divImage5.appendChild(image5);
 divContent5.appendChild(paragraph5);
